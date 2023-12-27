b34ec4746d3c6938dfddbdccee61c89546fdb66b
Hamza Barrak <hbarrak@c3r5p7.1337.ma>
2023-12-27 15:42:41 +0100

    fixed location matching

--- src/config_parse/T_server.cpp
+++ src/config_parse/T_server.cpp
@@ -12,9 +12,34 @@ std::string t_server::getErrorPage(std::string& errn_){
 bool t_server::hasLocation(std::string& location){
     if (locations.find(location) != locations.end())
         return 1;
+
+    std::map<std::string, t_location>::iterator it = locations.begin();
+
+    for (it; it != locations.end(); it++){
+        int i = 0;
+
+        while (location[i] && it->first[i] && location[i] == it->first[i])
+            i++;
+        if (!location[i] || !it->first[i])
+            return 1;
+    }
     return 0;
 }
 
 t_location& t_server::getLocation(std::string& location){
-    return locations[location];
+    std::map<std::string, t_location>::iterator it = locations.begin();
+    t_location& re;
+    int max_match = 0;
+
+    for (it; it != locations.end(); it++){
+        int i = 0;
+
+        while (location[i] && it->first[i] && location[i] == it->first[i])
+            i++;
+        if ((!location[i] || !it->first[i]) && i > max_match){
+            max_match = i;
+            re = it->second;
+        }
+    }
+    return re;
 }
