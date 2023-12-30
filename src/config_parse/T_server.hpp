3e2945de0b3a5d0eab769d7c6a6e783b5dc40547
Hamza Barrak <hbarrak@c3r5p7.1337.ma>
2023-12-30 11:49:10 +0100

    made attributes private

--- src/config_parse/T_server.hpp
+++ src/config_parse/T_server.hpp
@@ -4,15 +4,16 @@
 #include "T_location.hpp"
 #include "../utils/utils.hpp"
 
-class t_server {
+class t_server {        
+    std::map<std::string, t_location>   locations;
+    std::map<std::string, std::string>  error_pages;
     public:
         int                                 port;    
         std::string                         ip;
         std::string                         server_name;
         size_t                              max_body_size;
         std::string                         body_size;
-        std::map<std::string, t_location>   locations;
-        std::map<std::string, std::string>  error_pages;
+
 
         std::vector<std::string> getErrorPage(std::string& errn_);
         bool hasLocation(std::string& location);
