5c3261f39e31538c8830f7a1ecf9f6f9d269644a
Ahmed AITLFDIL <afadilahmad7@gmail.com>
2024-01-11 00:40:19 +0100

    Refactor ClientHandler::SendResponse() method and improve code formatting

--- src/ClientHandler/SendResponse.cpp
+++ src/ClientHandler/SendResponse.cpp
@@ -4,7 +4,7 @@
 void ClientHandler::SendResponse(){
 	if (!headersSent){
 		if (file.empty()){
-            std::string errorPage = serverConfig.getErrorPage(statusCode);
+			std::string errorPage = serverConfig.getErrorPage(statusCode);
 			file = errorPage;
 		}
 		std::string re;
@@ -16,24 +16,23 @@ void ClientHandler::SendResponse(){
 
 		headersSent = 1;
 	}
-	else{		
-		//protect	
-		std::ifstream fileToSend(file.c_str(), std::ios::binary);        
-		char buffer[BUFFER_SIZE + 1] =  {0};        
+	else{
+		//protect
+		std::ifstream fileToSend(file.c_str(), std::ios::binary);
+		char buffer[BUFFER_SIZE + 1] =  {0};
 
 		fileToSend.seekg(offset);
 		fileToSend.read(buffer, BUFFER_SIZE);
 		offset = fileToSend.tellg();
 		// check g count, protect
-		int readLen = fileToSend.gcount();	
-	
+		int readLen = fileToSend.gcount();
+
 		sendingBuffer.append((unsigned const char*)buffer, readLen);
 		sendToSocket();
 
 		/// set status to Closed if done sending
 		if (!readLen)
 		{
-			//sleep(2);
 			headersSent = 0;
 			status = Closed;
 		}
@@ -46,7 +45,7 @@ std::string ClientHandler::generateHeaders(){
 	std::string re;
 
 	re = "HTTP/1.1 " + statusCode + " " + statusString + "\r\n";
-	re += "Content-Type: " + getMimeType(getExtension()) + "\r\n";		
+	re += "Content-Type: " + getMimeType(getExtension()) + "\r\n";
 	re += "Content-Length: " + getContentLength() + "\r\n";
 	re += extraHeaders;
 	//if (!isCGI)
@@ -62,41 +61,41 @@ std::string ClientHandler::getExtension(){
 }
 
 std::string ClientHandler::getContentLength(){
-	std::stringstream ss;	
+	std::stringstream ss;
 	std::ifstream tmpifstream(file.c_str(), std::ios::binary);
 	tmpifstream.seekg(0, std::ios::end);
 	ss << tmpifstream.tellg();
 	tmpifstream.close();
-    return ss.str();
+	return ss.str();
 }
 
 std::string ClientHandler::getMimeType(std::string ext){
 	std::map<std::string, std::string> mimeTypes;
-    
-    mimeTypes["txt"] = "text/plain";
-    mimeTypes["html"] = "text/html";
-    mimeTypes["css"] = "text/css";
-    mimeTypes["js"] = "application/javascript";
-    mimeTypes["jpg"] = "image/jpeg";
-    mimeTypes["webp"] = "image/webp";
-    mimeTypes["png"] = "image/png";
-    mimeTypes["gif"] = "image/gif";
-    mimeTypes["pdf"] = "application/pdf";
-    mimeTypes["doc"] = "application/msword";
-    mimeTypes["xml"] = "application/xml";
-    mimeTypes["json"] = "application/json";
-    mimeTypes["mp3"] = "audio/mpeg";
-    mimeTypes["csv"] = "text/csv";
-    mimeTypes["zip"] = "application/zip";
-    mimeTypes["tar"] = "application/x-tar";
-    mimeTypes["mp4"] = "video/mp4";
-    mimeTypes["ogg"] = "audio/ogg";
-    mimeTypes["svg"] = "image/svg+xml";
-    mimeTypes["xls"] = "application/vnd.ms-excel";
-    mimeTypes["ppt"] = "application/vnd.ms-powerpoint";
-    mimeTypes["ico"] = "image/x-icon";
-    mimeTypes["woff"] = "font/woff";
-    mimeTypes["mpg"] = "video/mpeg";
+
+	mimeTypes["txt"] = "text/plain";
+	mimeTypes["html"] = "text/html";
+	mimeTypes["css"] = "text/css";
+	mimeTypes["js"] = "application/javascript";
+	mimeTypes["jpg"] = "image/jpeg";
+	mimeTypes["webp"] = "image/webp";
+	mimeTypes["png"] = "image/png";
+	mimeTypes["gif"] = "image/gif";
+	mimeTypes["pdf"] = "application/pdf";
+	mimeTypes["doc"] = "application/msword";
+	mimeTypes["xml"] = "application/xml";
+	mimeTypes["json"] = "application/json";
+	mimeTypes["mp3"] = "audio/mpeg";
+	mimeTypes["csv"] = "text/csv";
+	mimeTypes["zip"] = "application/zip";
+	mimeTypes["tar"] = "application/x-tar";
+	mimeTypes["mp4"] = "video/mp4";
+	mimeTypes["ogg"] = "audio/ogg";
+	mimeTypes["svg"] = "image/svg+xml";
+	mimeTypes["xls"] = "application/vnd.ms-excel";
+	mimeTypes["ppt"] = "application/vnd.ms-powerpoint";
+	mimeTypes["ico"] = "image/x-icon";
+	mimeTypes["woff"] = "font/woff";
+	mimeTypes["mpg"] = "video/mpeg";
 
 	std::map<std::string,std::string>::iterator it = mimeTypes.find(ext);
 	if (it != mimeTypes.end())
@@ -105,10 +104,10 @@ std::string ClientHandler::getMimeType(std::string ext){
 }
 
 void ClientHandler::setResponseParams(std::string statusCode, std::string statusString, std::string extraHeaders, std::string file, bool isCGI){
-    this->file = file;
-    this->statusCode = statusCode;
-    this->extraHeaders = extraHeaders;
+	this->file = file;
+	this->statusCode = statusCode;
+	this->extraHeaders = extraHeaders;
 	this->statusString = statusString;
-    this->isCGI = isCGI;
+	this->isCGI = isCGI;
 	status = Sending;
 }
