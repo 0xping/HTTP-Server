90b96969be1cf7d768cc9c312d297e3b1c1e7356
0xping <afadilahmad7@gmail.com>
2024-01-22 15:49:33 +0100

    Refactor postHtmlResponseGenerator and replaceNewlineWithLiteral functions

--- utils/PostUtils.cpp
+++ utils/PostUtils.cpp
@@ -9,41 +9,54 @@ bool isValidBase(const std::string& number, unsigned int &size, const int base)
     return true;
 }
 
+
 std::string postHtmlResponseGenerator(std::vector<std::string>& filesNames)
 {
-    std::string htmlResp = "";
-    htmlResp += "<!DOCTYPE html>";
-    htmlResp += "<html>";
-    htmlResp += "<head>";
-    htmlResp += "<title>File Upload Success</title>";
-    htmlResp += "</head>";
-    htmlResp += "<body>";
-    if (filesNames.size() > 2)
-    {
-        htmlResp += "<h1>Files Uploaded Successfully!</h1>";
-        htmlResp += "<p>The following files have been uploaded:</p>";
-    }
-    else
-    {
-        htmlResp += "<h1>File Uploaded Successfully!</h1>";
-        htmlResp += "<p>The following file have been uploaded:</p>";
-    }
-    htmlResp += "<ul>";
-    for (size_t i = 0; i < filesNames.size() - 1; i++)
-        htmlResp += "<ul>" + filesNames[i] + "</ul>";
-    
-    htmlResp += "</ul>";
-    htmlResp += "</body>";
+    std::string htmlResp = ""
+    "<!DOCTYPE html>"
+    "<html lang=\"en\">"
+    "<head>"
+        "<meta charset=\"UTF-8\">"
+        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+        "<title>File Upload Success</title>"
+        "<style>"
+            "body {"
+                "background-color: #212121;"
+                "color: #ffffff;"
+                "font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;"
+                "text-align: center;"
+                "padding: 50px;"
+                "margin: 0;"
+            "}"
+            ".success-message {"
+                "background-color: #43a047;"
+                "color: #ffffff;"
+                "padding: 20px;"
+                "border-radius: 5px;"
+                "box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);"
+            "}"
+        "</style>"
+    "</head>"
+    "<body>"
+        "<div class=\"success-message\">"
+            "<h1>File Uploaded Successfully!</h1>"
+            "<p>The following files has been successfully uploaded to the server.</p>"
+            "<ul>";
+                for (size_t i = 0; i < filesNames.size() - 1; i++)
+                    htmlResp += "<li> <a href=" + (std::string)(filesNames[i].c_str() + 6) + ">"  + filesNames[i] + "</a></li>" ;
+            htmlResp += "</ul>"
+        "</div>"
+    "</body>";
     htmlResp += "</html>";
     return htmlResp;
 }
 
-std::string replaceNewlineWithLiteral(const std::string &input) 
+std::string replaceNewlineWithLiteral(const std::string &input)
 {
     std::string result = input;
     size_t pos = 0;
 
-    while ((pos = result.find("\r\n", pos)) != std::string::npos) 
+    while ((pos = result.find("\r\n", pos)) != std::string::npos)
     {
         result.replace(pos, 2, "\\r\\n");
         pos += 4; // Advance position (skip replaced string)
